ebdb709beecdf3c7e61b70bbe41ffe4d1d218af2
sgoss11 <77243700+sgoss11@users.noreply.github.com>
2021-05-02 18:52:13 -0800

    WayPoiny Update
    Waypoints now stay in location when they shake

--- Assets/Scripts/WayPoint/WayPoint.cs
+++ Assets/Scripts/WayPoint/WayPoint.cs
@@ -9,7 +9,11 @@ public class WayPoint : MonoBehaviour
     private const int kHitLimit = 3;
     private const float kRepositionRange = 15f; // +- this value
     private Color mNormalColor = Color.white;
-
+    
+    //Shake
+    Vector3 init;
+    public float mag = 0f;
+    public float dur = 0f;
 
     SpriteRenderer Wp;
     private float alphaC = 0.25f;
@@ -23,13 +27,12 @@ public class WayPoint : MonoBehaviour
 
     private void Reposition() 
     {
-        Vector3 p = mInitPosition;
+       Vector3 p = mInitPosition;
         p += new Vector3(Random.Range(-kRepositionRange, kRepositionRange),
                          Random.Range(-kRepositionRange, kRepositionRange),
                          0f);
         transform.position = p;
-        alphaC = 1.0f;
-        Wp.color = new Color(1.0f, 1.0f, 1.0f, alphaC);
+        Wp.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -42,7 +45,7 @@ public class WayPoint : MonoBehaviour
             mHitCount++;
             if (mHitCount == 1)
             {
-                StartCoroutine(Shake(1, 1));
+              StartCoroutine(Shake(1, 1));
             }
             else if (mHitCount == 2)
             {
@@ -50,7 +53,7 @@ public class WayPoint : MonoBehaviour
             }
             else if (mHitCount == 3)
             {
-                StartCoroutine(Shake(3, 3));
+                 StartCoroutine(Shake(3, 3));
             }
             else if (mHitCount == 4)
             {
@@ -66,20 +69,21 @@ public class WayPoint : MonoBehaviour
 
     IEnumerator Shake(float dur, float mag)
     {
-        Vector3 orig = transform.position;
+        Vector2 orig = transform.position;
 
         float elapseT = 0.0f;
+        dur = dur/2;
 
         while(elapseT < dur)
         {
-            float x = Random.Range(-1f, 1f) * mag;
-            float y = Random.Range(-1f, 1f) * mag;
+            float x = orig.x + (Random.Range(0f, 1f) * mag);
+            float y = orig.y + (Random.Range(0f, 1f) * mag);        
 
-            transform.localPosition = new Vector3(x, y, orig.z);
-            elapseT += Time.smoothDeltaTime;
-            yield return null;
+           transform.position = new Vector2(x, y);
+           elapseT += Time.smoothDeltaTime;
+           yield return null;
         }
-        transform.localPosition = orig;
+        transform.position = orig;
 
     }
 }
